707dfa4799c7fafdddb4377e04cba08f292ff569
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-20 10:48:34 -0500

    second commit make sure netify is working and updating

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Starting deployment</p>
+        <p>Deployed</p>
       </header>
     </div>
   );
