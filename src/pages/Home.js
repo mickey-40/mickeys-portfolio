43814432bc5948eb6da2d90631ef73818f8cca49
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-20 20:53:40 -0500

    set up home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedin, faGithub, faFacebook} from '@fortawesome/free-brands-svg-icons'
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2>Hi, My name is Mickey</h2>
+        <div className='prompt'>
+          <p>Introduction paragraph. Fill out later.</p>
+          <div className='socalMedia'><FontAwesomeIcon icon={faTwitter} />
+          <FontAwesomeIcon icon={faLinkedin} />
+          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faFacebook} />
+          </div>
+        </div>
+      </div>
+      <div className='skills'>
+
+        <h1>Skills</h1>
+        <ol className='list'>
+          <li className='item'>
+            <h2>Front-End</h2>
+            <span>ReactJS, Redux, HTML, CSS, NPM, BootStrap, StyledComponents</span>
+          </li>
+          <li className='item'>
+            <h2>Back-End</h2>
+            <span>NodeJS, ExpressJS, GraphQL, MySQL, MongoDB</span>
+          </li>
+          <li className='item'>
+            <h2>Langages</h2>
+            <span>JavaScript, Python</span>
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
