31821b915b4eb1de23c0eb0bc2530d757dcfbf43
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-22 11:18:35 -0500

    changed styling for experience.js

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -9,29 +9,38 @@ function Experience() {
     <div className='experience'>
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement 
-          className='vertical-timeline-element--education' 
+          className='vertical-timeline-element--work' 
           date='2000-2012' 
+          contentStyle={{ background: '#3e497a', color: '#000' }}
+          contentArrowStyle={{ borderRight: '7px solid  #3e497a' }}
           iconStyle={{background: '#3e497a', color: '#fff'}}
           icon={<FontAwesomeIcon icon={faBolt} />}>
-          <p>Electrican at Equipment Management Services</p>
+          <h3 className="vertical-timeline-element-title">Electrican</h3>
+          <p className="vertical-timeline-element-subtitle">Company: Equipment Management Services Location: Austin, Tx</p>
         </VerticalTimelineElement>        
       </VerticalTimeline>
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement 
           className='vertical-timeline-element--education' 
+          contentStyle={{ background: '#e9d35b', color: '#000' }}
+          contentArrowStyle={{ borderRight: '7px solid  #3e497a' }}
           date='2012-2016' 
           iconStyle={{background: '#e9d35b', color: '#3e497a'}}
           icon={<FontAwesomeIcon icon={faSchool} />}>
-          <p>Degree Interdisciplinary Studies (BS) from University of Houston-Downtown</p>
+          <h3 className="vertical-timeline-element-title">University of Houston-Downtown</h3>
+          <p className="vertical-timeline-element-subtitle">Degree in Interdisciplinary Studies (BS)</p>
         </VerticalTimelineElement>        
       </VerticalTimeline>
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement 
-          className='vertical-timeline-element--education' 
+          className='vertical-timeline-element--work' 
+          contentStyle={{ background: '#3e497a', color: '#000' }}
+          contentArrowStyle={{ borderRight: '7px solid  #3e497a' }}
           date='2016-Current' 
           iconStyle={{background: '#3e497a', color: '#fff'}}
           icon={<FontAwesomeIcon icon={faPersonChalkboard} />}>
-          <p>Middle School Math Teacher with Houston ISD and Spring Branch ISD</p>
+          <h3 className="vertical-timeline-element-title">Teacher</h3>
+          <p className="vertical-timeline-element-subtitle">Middle School Math Teacher with Houston ISD and Spring Branch ISD</p>
         </VerticalTimelineElement>        
       </VerticalTimeline>
      
