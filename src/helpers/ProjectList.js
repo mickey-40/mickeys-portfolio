22dd1d1003c902c0a4fb054f1371bb3102ae027b
mickey-40 <mickeyarnold53@yahoo.com>
2024-11-05 08:04:37 -0600

    took out project images not used

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,10 +1,10 @@
 import Proj1 from '../assets/Proj1.png'
 import Proj2 from '../assets/Proj2.jpg'
 import Proj3 from '../assets/Proj3.jpg'
-import Proj4 from '../assets/Proj4.jpg'
+// import Proj4 from '../assets/Proj4.jpg'
 import Proj5 from '../assets/Proj5.jpg'
 import Proj6 from '../assets/Proj6.jpeg'
-import Proj7 from '../assets/Proj7.jpeg'
+// import Proj7 from '../assets/Proj7.jpeg'
 
 
 export const ProjectList = [
